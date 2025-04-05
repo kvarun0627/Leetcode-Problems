812a5104a5429978a5d53cfcbaed5335831def67
kvarun0627 <165155953+kvarun0627@users.noreply.github.com>
2025-04-05 22:04:57 +0530

    Time: 223 ms (56.23%), Space: 101.6 MB (61%) - LeetHub

---
+++ 3393-count-paths-with-the-given-xor-value/3393-count-paths-with-the-given-xor-value.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int mod=1e9+7;
+    int DP_memo(vector<vector<int>>& grid, int k,int x,int y,int val,int m,int n, vector<vector<vector<int>>>& DP)
+    {   
+        val^=grid[x][y];
+        if(x==m-1 && y==n-1) return val==k?1:0;
+
+        if(DP[x][y][val]!=-1) return DP[x][y][val];
+
+        int right=0;int down=0;
+        
+        if(x+1<m)
+        {
+            down=DP_memo(grid,k,x+1,y,val,m,n,DP)%mod;
+        }
+        if(y+1<n)
+        {
+            right=DP_memo(grid,k,x,y+1,val,m,n,DP)%mod;
+        }
+        return DP[x][y][val]=(right+down)%mod;
+    }
+    int countPathsWithXorValue(vector<vector<int>>& grid, int k) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<vector<int>>>DP(m,vector<vector<int>>(n,vector<int>(16,-1)));
+        return DP_memo(grid,k,0,0,0,m,n,DP);
+    }
+};
