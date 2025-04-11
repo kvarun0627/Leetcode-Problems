42aba6680150fab54a48835ed7a38098251d9d79
kvarun0627 <165155953+kvarun0627@users.noreply.github.com>
2025-04-11 11:39:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
-    int reccur(string&s,string&t,int i,int j,vector<vector<int>>&DP)
-    {
-        if(j<0) return 1;
-        if(i<0) return 0;
-
-        if(DP[i][j]!=-1) return DP[i][j];
-
-        int skip=reccur(s,t,i-1,j,DP);
-        int not_skip=0;
-        if(s[i]==t[j])
-        {
-            not_skip=reccur(s,t,i-1,j-1,DP);
-        }
-        return DP[i][j]=skip+not_skip;
-    }
     int numDistinct(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        vector<vector<int>>DP(n,vector<int>(m,-1));
-        return reccur(s,t,n-1,m-1,DP);
+        int m = t.length(), n = s.length();
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
+        for (int j = 0; j <= n; j++) dp[0][j] = 1;
+        for (int j = 1; j <= n; j++)
+            for (int i = 1; i <= m; i++)
+                dp[i][j] = dp[i][j - 1] + (t[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : 0);
+        return dp[m][n];
     }
-};
+};  
