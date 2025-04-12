bd60a671eeed8efb0b86fd34439cce1bcfa869c7
kvarun0627 <165155953+kvarun0627@users.noreply.github.com>
2025-04-12 13:16:20 +0530

    Time: 0 ms (100%), Space: 16.1 MB (59.32%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int reccur(vector<int>&prices,vector<vector<int>>&DP,int cap,int idx,int n)
+    {
+        if(idx>=n) return 0;
+
+        int skip=reccur(prices,DP,cap,idx+1,n);
+
+        int buy=0;
+        int sell=0;
+        if(!cap)
+        {
+            buy=-prices[idx]+reccur(prices,DP,1,idx+1,n);
+            return max(buy,skip);
+        }
+        sell=prices[idx]+reccur(prices,DP,0,idx+2,n);
+        return max(skip,sell);
+
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>>DP(n+2,vector<int>(2,0));
+        for(int idx=n-1;idx>=0;idx--)
+        {
+            for(int cap=0;cap<=1;cap++)
+            {
+                int buy=0;
+                int sell=0;
+                int skip=DP[idx+1][cap];
+                if(!cap)
+                {
+                    buy=-prices[idx]+DP[idx+1][1];
+                    DP[idx][cap]=max(buy,skip);
+                }
+                else
+                {
+                    sell=prices[idx]+DP[idx+2][0];
+                    DP[idx][cap]=max(skip,sell);
+                }
+            }
+        }
+        return DP[0][0];
+    }
+};
