6eb84ec23bfef11dbe7425f10316d00333d2f7df
varunkrishna <165155953+kvarun0627@users.noreply.github.com>
2025-05-12 00:48:08 +0530

    Time: 0 ms (100%), Space: 8.5 MB (48.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
-    int memo(int n,vector<int>&DP)
-    {
-        if(n==1 || n==0) return 1;
-        
-        if(DP[n]!=-1) return DP[n];
+    
+    //multiplication of 2 x 2 matrix
+    void multiply(vector<vector<int>>&fibMatrix,vector<vector<int>>second) {
+        int a=(fibMatrix[0][0]*second[0][0] + fibMatrix[0][1]*second[1][0]);
+        int b=(fibMatrix[0][0]*second[0][1] + fibMatrix[0][1]*second[1][1]);
+        int c=(fibMatrix[1][0]*second[0][0] + fibMatrix[1][1]*second[1][0]);
+        int d=(fibMatrix[1][0]*second[0][1] + fibMatrix[1][1]*second[1][1]);
 
-        return DP[n]=memo(n-1,DP)+memo(n-2,DP);
+        fibMatrix[0][0]=a;
+        fibMatrix[0][1]=b;
+        fibMatrix[1][0]=c;
+        fibMatrix[1][1]=d;
     }
+
+    //raise the matrix to the power 'n'
+    void power(vector<vector<int>>&fibMatrix, int n) {
+        if (n==0 || n==1) return;
+
+        power(fibMatrix,n/2);//recursively divide the power
+        multiply(fibMatrix,fibMatrix);//square the matrix
+        
+        if(n%2!=0)//if n is odd, multiply once more
+        {
+            vector<vector<int>>baseMatrix={{1,1},{1,0}};
+            multiply(fibMatrix,baseMatrix);
+        }   
+    }
+
     int climbStairs(int n) {
-        vector<int>DP(n+1,-1);
-        return memo(n,DP);
+        if (n==0) return 1;
+        vector<vector<int>>fibMatrix={{1,1},{1,0}};
+        power(fibMatrix,n);
+
+        return fibMatrix[0][0];
     }
 };
